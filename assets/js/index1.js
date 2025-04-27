8bcf7dbf7c6a1145555c61e4dcd27d7d09c001b3
Deidy Rivas <Deidy5002@gmail.com>
2025-04-27 14:45:05 -0400

    Fase1 completada

---
+++ assets/js/index1.js
@@ -0,0 +1,10 @@
+const imagen = document.getElementById("imagen");
+
+imagen.addEventListener("click" , function() {
+    if (this.style.border) {
+        this.style.border = "";
+    } else {
+        this.style.border = "2px solid red"; 
+
+    }
+})
