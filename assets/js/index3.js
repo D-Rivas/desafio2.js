ce305250542ef7d441412a6910ac5b65e1dc51b7
Deidy Rivas <Deidy5002@gmail.com>
2025-04-28 19:46:02 -0400

    últimos detalles

--- assets/js/index3.js
+++ assets/js/index3.js
@@ -1,20 +1,20 @@
-const digit1 = document.querySelector('#digit1');
-const digit2 = document.querySelector('#digit2');
-const digit3 = document.querySelector('#digit3');
-const button = document.querySelector('#checkPassword');
-const result = document.querySelector('#result');
+const select1 = document.querySelector('#digit1');
+const select2 = document.querySelector('#digit2');
+const select3 = document.querySelector('#digit3');
+const button = document.querySelector('#check');
+const message = document.querySelector('#message');
 
 button.addEventListener('click', () => {
-    const password = digit1.value + digit2.value + digit3.value;
+    const password = select1.value + select2.value + select3.value;
 
     if (password === '911') {
-        result.textContent = 'password 1 correcto';
-        result.style.color = 'green';
+        message.textContent = "password 1 correcto";
+        message.style.color = "green";
     } else if (password === '714') {
-        result.textContent = 'password 2 correcto';
-        result.style.color = 'green';
+        message.textContent = "password 2 correcto";
+        message.style.color = "green";
     } else {
-        result.textContent = 'password incorrecto';
-        result.style.color = 'red';
+        message.textContent = "password incorrecto";
+        message.style.color = "red";
     }
 })
